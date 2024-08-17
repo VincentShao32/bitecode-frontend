95abb452426507b6bdcae27ec6af7051da92eedf
Ivan Kwok <ivnkwok@gmail.com>
2024-08-17 11:51:01 -0700

    Uniform Buttons
    made the buttons uniform

--- src/components/navbar/NavbarTab.js
+++ src/components/navbar/NavbarTab.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const NavbarTab = ({ text, path }) => {
   return (
     <Link
-      className="text-center bg-slate-300 h-max p-2 rounded w-[25%]"
+      className="text-center place-content-center bg-slate-300 h-vh p-2 rounded w-[25%]"
       to={path}
     >
       {text}
