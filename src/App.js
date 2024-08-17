52680fd7058172acd9b8746318b81a5c65cfedc7
VincentShao32 <yunbo2016@gmail.com>
2024-08-17 09:55:14 -0700

    tailwind setup + delete some boilerplate code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-slate-600 min-h-screen p-20">
+      <h1 className="text-center text-white text-6xl">Bitecode!</h1>
     </div>
   );
 }
